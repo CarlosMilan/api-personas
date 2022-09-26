f193051f506e7d4f92d07d44da4b5f1b02d97fae
CarlosMilan <martin.milan3@gmail.com>
2022-09-26 10:24:33 -0300

    Se crea entidad Persona y se configura bd

--- src/main/java/com/gl/cm/personas/model/Persona.java
+++ src/main/java/com/gl/cm/personas/model/Persona.java
@@ -1,2 +1,91 @@
-package com.gl.cm.personas.model;public class Persona {
+package com.gl.cm.personas.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "personas")
+public class Persona {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "uuid")
+    private UUID id;
+
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private LocalDate fechaNacimiento;
+    private LocalDateTime creacion;
+    private Boolean activo;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public LocalDateTime getCreacion() {
+        return creacion;
+    }
+
+    public void setCreacion(LocalDateTime creacion) {
+        this.creacion = creacion;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
